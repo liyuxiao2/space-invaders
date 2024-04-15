ef91126b3096ea6d55e7da08f455e3de3dfc177e
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2024-04-14 20:46:16 -0400

    added asset

--- game.py
+++ game.py
@@ -1,12 +1,21 @@
 import pygame
 import Actors.Player as Player
+import os
 
 pygame.init()
 
 screen_width = 800
 screen_height = 800
 
+
+
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+
+image = pygame.image.load('Assets/Images/Running1.png')  # Replace 'your_image_file.png' with the path to your image file
+
+# Scale the image to match the size of the rectangle
+p1_image = pygame.transform.scale(image, (50, 50))  # Adjust the size as needed
 
 
 p1 = Player.Player(50,50,screen_width/2,screen_height/2, 15)
@@ -31,7 +40,7 @@ while(run):
     screen.fill((0,0,0)) 
 
     #creates playe
-    pygame.draw.rect(screen, (255,0,0), p1)
+    screen.blit(p1_image, p1)
         
     pygame.display.update()
 
